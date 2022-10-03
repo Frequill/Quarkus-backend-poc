04755924597e6a3be486812463f60f4bcc6843b6
julius <julius.thomsen99@gmail.com>
2022-10-03 15:45:18 +0200

    added test case for "login" endpoint

--- src/test/java/org/acme/MainEndpointsTest.java
+++ src/test/java/org/acme/MainEndpointsTest.java
@@ -1,10 +1,13 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 
 @QuarkusTest
 public class MainEndpointsTest {
@@ -12,10 +15,24 @@ public class MainEndpointsTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/myPath")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .when().get("/myPath")
+                .then()
+                .statusCode(200)
+                .body(is("Hello from RESTEasy Reactive"));
     }
+
+    @Test
+    public void testLoginEndpoint() {
+        given()
+                .when().accept(ContentType.ANY).contentType(ContentType.JSON).body("{\"username\":\"julius\",\"password\":\"foobar\"}").post("/myPath/login")
+                .then()
+                .statusCode(200)
+                // Test with substring so that EXACT currentTime is not needed
+                .body(containsString("Status: 0, Username: julius"));
+    }
+
+
+
+
 
 }
